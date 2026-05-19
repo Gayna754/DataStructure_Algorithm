f4e2a38150833367efd92479323ba7f84ba2bbf7
Gayna Chawla <gaynachawla@gmail.com>
2026-05-19 21:51:34 +0530

    updated array

--- public/Array.cpp
+++ public/Array.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <vector>
 #include <unordered_map>
+#include<unordered_set>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -217,7 +219,48 @@ vector<int> rearrangeArray(vector<int>& nums) {
     
     return res;
 }
+// longest consequent subsequence
+ int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>st(nums.begin(),nums.end());
+        int longest=0;
+        for(int x:st){
+           if( st.find(x-1)==st.end()){
+            int curr=x;
+            int length=0;
+            while(st.find(curr)!=st.end()){
+                curr++;
+                length++;
+            }longest=max(length,longest);
+        } }return longest;
+        
+        
+    }
+    // next permutation
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int idx=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>=idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx+1,nums.end());
 
+
+
+        
+    }
 
 
 
