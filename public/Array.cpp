5b9237a66561b3cc740e1d7c63dd044d9e677cf7
Gayna Chawla <gaynachawla@gmail.com>
2026-05-21 19:44:45 +0530

    updated Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -11,6 +11,32 @@ class Solution {
 
 
 public:
+vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0&&nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int target=nums[i]+nums[j]+nums[k];
+                if(target>0){
+                    k--;
+                }else if(target<0){
+                    j++;
+                }else{
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }return ans;
+        
+    }
  vector<int>generaterow(int row){
     long long ans=1;
     
