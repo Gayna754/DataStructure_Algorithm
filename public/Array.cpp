cd42569228211ecb01277fc78df3dfebc6f5319e
Gayna Chawla <gaynachawla@gmail.com>
2026-05-21 16:53:59 +0530

    updated Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -11,6 +11,42 @@ class Solution {
 
 
 public:
+ vector<int>generaterow(int row){
+    long long ans=1;
+    
+    vector<int>Ansrow;
+    Ansrow.push_back(1);
+    for(int col=1;col<row;col++){
+        ans=ans*(row-col);
+        ans=ans/col;
+        Ansrow.push_back(ans);
+    }
+    return Ansrow;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(generaterow(i) );}
+
+      
+     return ans;}
+        
 vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
