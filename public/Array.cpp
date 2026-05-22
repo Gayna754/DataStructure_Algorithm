7bf979d590d4f4411f10935a13aa863124ade94d
Gayna Chawla <gaynachawla@gmail.com>
2026-05-22 21:05:47 +0530

    updated Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -11,6 +11,21 @@ class Solution {
 
 
 public:
+int removeDuplicates(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+
+   
+    int j = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        if (arr[i] != arr[i + 1])
+            arr[j++] = arr[i];
+
+    arr[j++] = arr[n - 1];
+    return j;
+}
 int getMin(int arr[], int n)
 {
     
