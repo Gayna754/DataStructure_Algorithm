cb5be45ce9ee39dd024fb16c9e1a2a517507596c
Gayna Chawla <gaynachawla@gmail.com>
2026-05-22 21:00:04 +0530

    updated Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -13,6 +13,30 @@ class Solution {
 public:
 int getMin(int arr[], int n)
 {
+    
+    return (n == 1) ? arr[0] : min(arr[0], 
+                         getMin(arr + 1, n - 1));
+}
+
+int getMax(int arr[], int n)
+{
+    
+    return (n == 1) ? arr[0] : max(arr[0], 
+                          getMax(arr + 1, n - 1));
+}
+
+int main()
+{
+    int arr[] = { 12, 1234, 45, 67, 1 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Minimum element of array: " << 
+                            getMin(arr, n) << " ";
+    cout << "Maximum element of array: " << 
+                                   getMax(arr, n);
+    return 0;
+}
+int getMin(int arr[], int n)
+{
     int res = arr[0];
     for (int i = 1; i < n; i++)
         res = min(res, arr[i]);
