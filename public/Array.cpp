ae310cc00ce83005b28cd1f590c97f9f00366064
Gayna Chawla <gaynachawla@gmail.com>
2026-05-22 21:10:06 +0530

    updated Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -11,6 +11,29 @@ class Solution {
 
 
 public:
+
+int removeDuplicates(int arr[], int n)
+{
+    // Return, if array is empty or contains a single element
+    if (n == 0 || n == 1)
+        return n;
+
+    int temp[n];
+
+    int j = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        if (arr[i] != arr[i + 1])
+            temp[j++] = arr[i];
+
+    temp[j++] = arr[n - 1];
+
+    
+    for (int i = 0; i < j; i++)
+        arr[i] = temp[i];
+
+    return j;
+}
 int removeDuplicates(int arr[], int n)
 {
     if (n == 0 || n == 1)
