93a715e9f1f6dbcdb3aa1fdac539574269c5ef31
Gayna Chawla <gaynachawla@gmail.com>
2026-05-22 20:03:54 +0530

    updated array

--- public/Array.cpp
+++ public/Array.cpp
@@ -11,6 +11,68 @@ class Solution {
 
 
 public:
+ long merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+
+        int left = low;
+        int right = mid + 1;
+
+        long long cnt = 0;
+
+        while (left <= mid && right <= high) {
+
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+
+                cnt += (mid - left + 1);
+
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+
+        return cnt;
+    }
+
+    long long mergeSort(vector<int>& arr, int low, int high) {
+
+        if (low >= high)
+            return 0;
+
+        int mid = (low + high) / 2;
+
+        long long cnt = 0;
+
+        cnt += mergeSort(arr, low, mid);
+
+        cnt += mergeSort(arr, mid + 1, high);
+
+        cnt += merge(arr, low, mid, high);
+
+        return cnt;
+    }
+
+    long long inversionCount(vector<int>& arr) {
+
+        return mergeSort(arr, 0, arr.size() - 1);
+    }
 vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
         int n=nums.size();
