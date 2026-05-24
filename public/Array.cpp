368e48e2fd6ed8e9bdcffc74c35642d21372af1f
Gayna Chawla <gaynachawla@gmail.com>
2026-05-24 23:09:59 +0530

    Updayed Array

--- public/Array.cpp
+++ public/Array.cpp
@@ -13,6 +13,32 @@ class Solution {
 
 
 public:
+
+ int maxProduct(vector<int>& nums) {
+        
+
+        int ans = INT_MIN;
+        int product = 1;
+
+       
+        for(int i = 0; i < nums.size(); i++) {
+            product *= nums[i];
+            ans = max(ans, product);
+            if(product == 0)
+                product = 1;
+        }
+
+        
+        product = 1;
+        for(int j = nums.size() - 1; j >= 0; j--) {
+            product *= nums[j];
+            ans = max(ans, product);
+            if(product == 0)
+                product = 1;
+        }
+        
+    return ans;
+    }
  int maxProduct(vector<int>& nums) {
         
 
