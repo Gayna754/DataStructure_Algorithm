55ec6e8e93f84eb46acf4b966e8a79b64d6df852
Gayna Chawla <gaynachawla@gmail.com>
2026-05-25 22:13:45 +0530

    updated Arra

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -9,6 +9,37 @@ using namespace std;
 
 class Solution {
     public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        int j=0;
+        int i=0;
+        queue<int>q;
+        vector<int>temp;
+       
+        while(j<arr.size()){
+            if(arr[j]<0){
+            q.push(arr[j]);
+            
+        }
+        
+        if(j-i+1<k){
+            j++;
+        }else if(j-i+1==k){
+            
+        
+       if (q.empty()){temp.push_back(0);}
+       else{temp.push_back(q.front());}
+        
+        if(!q.empty()&&arr[i]==q.front()){
+            q.pop();
+            
+        }
+        i++;
+        j++;
+        }}
+        return temp;
+         
+         
+    }
 
     int maxSubarraySum(vector<int>& arr, int k) {
         int ans=INT_MIN;
