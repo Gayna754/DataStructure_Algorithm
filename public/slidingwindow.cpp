833cc7f00d488d93b633fd53be448c5322bd245a
Gayna Chawla <gaynachawla@gmail.com>
2026-05-27 14:51:48 +0530

    sliding

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -9,6 +9,34 @@ using namespace std;
 
 class Solution {
     public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+       
+        long long ans=0;
+        int i=0,j=0;
+        long long sum=0;
+        while(j<nums.size()){
+            sum+=nums[j];
+            mp[nums[j]]++;
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                if(mp.size()==k){
+                ans=max(ans,sum);}
+            mp[nums[i]]--;
+            if(mp[nums[i]]==0){
+                mp.erase(nums[i]);
+            }
+            sum-=nums[i];
+            i++;
+            j++;}
+
+
+        }return ans;
+        
+        
+    }
     int search(string &pat, string &txt) {
         unordered_map<int,int>mp;
         for(char ch:pat){
@@ -43,6 +71,7 @@ class Solution {
             i++;
             j++;}
         }return ans;
+        
         
        
         
