f16f7cc7e8013f8a57650113c3819e0bfaa93a8f
Gayna Chawla <gaynachawla@gmail.com>
2026-05-27 14:05:36 +0530

    sliding

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -9,6 +9,44 @@ using namespace std;
 
 class Solution {
     public:
+    int search(string &pat, string &txt) {
+        unordered_map<int,int>mp;
+        for(char ch:pat){
+            mp[ch]++;
+        }
+        int count=mp.size();
+        int k=pat.size();
+        int i=0,j=0;
+        
+        int ans=0;
+        while(j<txt.size()){
+            if(mp.find(txt[j])!=mp.end()){
+                mp[txt[j]]--;
+            
+            if(mp[txt[j]]==0){
+                count--;}
+            }
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                if(count==0){
+                    ans++;
+                }
+                if(mp.find(txt[i])!=mp.end()){
+                    if(mp[txt[i]]==0){
+                        count++;
+                    }
+                
+                mp[txt[i]]++;}
+            
+            i++;
+            j++;}
+        }return ans;
+        
+       
+        
+    }
     vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
 
         vector<int> ans;
