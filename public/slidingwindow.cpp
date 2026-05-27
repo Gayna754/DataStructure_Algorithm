9289b5e3e255edb1f06d342b47a69b8e0d99fa4c
Gayna Chawla <gaynachawla@gmail.com>
2026-05-27 23:27:54 +0530

    sliding

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -155,6 +155,26 @@ class Solution {
          
          
     }
+    int maxSubarraySum(vector<int>& arr, int k) {
+        int ans=INT_MIN;
+        int i=0;
+        int j=0;
+        int sum=0;
+        while(j<arr.size()){
+            sum+=arr[j];
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                ans=max(sum,ans);
+                sum-=arr[i];
+                i++;
+                j++;
+            }
+            }return ans;
+            
+       
+    }
+
 
     int maxSubarraySum(vector<int>& arr, int k) {
         int ans=INT_MIN;
