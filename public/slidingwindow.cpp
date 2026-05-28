d71eb4e24f4abb04c4a3f9199768464837a9eca2
Gayna Chawla <gaynachawla@gmail.com>
2026-05-28 21:53:26 +0530

    s

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -34,6 +34,34 @@ class Solution {
 
 
         }return ans;
+        public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        // Max heap storing {value, index}
+        priority_queue<pair<int,int>> pq;
+
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Push current element
+            pq.push({nums[i], i});
+
+            // Remove elements outside current window
+            while (!pq.empty() && pq.top().second <= i - k) {
+                pq.pop();
+            }
+
+            // Window of size k formed
+            if (i >= k - 1) {
+
+                // Top element is maximum
+                ans.push_back(pq.top().first);
+            }
+        }
+
+        return ans;
+    }
         
         
     }
