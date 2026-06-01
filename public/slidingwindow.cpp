d3f9f19d050e5257321715e8300de28f44dabb70
Gayna Chawla <gaynachawla@gmail.com>
2026-06-01 23:05:21 +0530

    s

--- public/slidingwindow.cpp
+++ public/slidingwindow.cpp
@@ -242,7 +242,7 @@ class Solution {
         
         int rev =0;
         
-        while(x!=1){
+        while(x!=2){
             
              if (rev > INT_MAX/10 || (rev == INT_MAX/10 && x%10 > 7)) return 0;
             if (rev < INT_MIN/10 || (rev == INT_MIN/10 && x%10 < -8)) return 0;          rev=rev*10+x%10;
