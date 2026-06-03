d92e4f578160871a99c6e05173411ec799101e53
Gayna Chawla <gaynachawla@gmail.com>
2026-06-03 22:38:17 +0530

    update

--- public/binarytree.cpp
+++ public/binarytree.cpp
@@ -143,5 +143,25 @@ public:
         return ans;
         
     }
+     vector<int> inOrder(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode*node=root;
+        vector<int>inorder;
+        while(node!=nullptr||!st.empty()){
+            if(node!=NULL){
+            st.push(node);
+            node=node->left;}
+            else{
+                if(st.empty())break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node=node->right;
+                
+            }
+        }return inorder;
+        
+        
+    }
 
 };
