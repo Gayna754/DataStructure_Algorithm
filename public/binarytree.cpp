27de808ffc50e2b9dafb40d70a604474d9fb594c
Gayna Chawla <gaynachawla@gmail.com>
2026-06-08 19:03:06 +0530

    tree

--- public/binarytree.cpp
+++ public/binarytree.cpp
@@ -659,4 +659,12 @@ void dfs(TreeNode*node,string path){
 
         return ans;
     }
+     int countNodes(TreeNode* root) {
+        if(root==nullptr)return 0;
+        int left=countNodes(root->left);
+        int right=countNodes(root->right);
+        return 1+ left+right;
+
+        
+    }
 };
