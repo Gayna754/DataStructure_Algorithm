33595636036e1419f46c8b939407db69a007feea
Gayna Chawla <gaynachawla@gmail.com>
2026-06-10 23:00:02 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
+
     int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int prev2 = 1;
+        int prev2 = 0;
         int prev1 = 2;
 
         for (int step = 3; step <= n; step++) {
