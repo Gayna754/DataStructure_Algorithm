a15f3bfb6bbcc6629ed13703cce4e04e748b1f49
Gayna Chawla <gaynachawla@gmail.com>
2026-06-14 16:53:09 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -121,4 +121,33 @@ public:
         }
     
         return prev;}
+        int findMaxSum(vector<int>& nums) {
+        int prev=nums[0];
+        int prev2=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1)pick+=prev2;
+            int notpick=0+prev;
+            int curr=max(pick,notpick);
+            prev2=prev;
+            prev=curr;
+           
+        }
+    
+        return prev;}
+        int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>temp1;
+        vector<int>temp2;
+        if(n==1)return nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(i!=0)temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+            
+
+
+        }
+        return max(findMaxSum(temp1),findMaxSum(temp2));
+    }
 };
