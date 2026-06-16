b8386814368339eced2164ccb36b64742b4b9dd6
Gayna Chawla <gaynachawla@gmail.com>
2026-06-16 14:02:26 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -223,4 +223,21 @@ public:
         }
         return ans;
     }
+    nt maxi(int ind,vector<int>&nums,vector<int>&dp){
+  if(ind<0)return 0;
+    if(ind==0)return nums[ind];
+    if(dp[ind]!=-1)return dp[ind];
+    int pick=nums[ind]+maxi(ind-2,nums,dp);
+    int notpick=0+maxi(ind-1,nums,dp);
+    return dp[ind]=max(pick,notpick);
+    
+}
+    int rob(vector<int>& nums) {
+         int n=nums.size();
+        vector<int>dp(n,-1);
+       
+        return maxi(n-1,nums,dp);
+        
+        
+    }
 };
