fea6826e98b8e0858a0ca07bd71843970b9efc98
Gayna Chawla <gaynachawla@gmail.com>
2026-06-16 23:52:42 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -275,7 +275,7 @@ public:
 
         dp[0][0] = max(mat[0][1], mat[0][2]);
         dp[0][1] = max(mat[0][0], mat[0][2]);
-        dp[0][2] = max(mat[0][0], mat[0][3]);
+        dp[0][2] = max(mat[0][0], mat[0][4]);
         dp[0][3] = max({mat[0][0], mat[0][1], mat[0][2]});
 
         for(int day = 1; day < n; day++) {
