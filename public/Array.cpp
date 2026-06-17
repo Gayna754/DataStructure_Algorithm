6b21324151f71802062d55b13927be08543a24e1
Gayna Chawla <gaynachawla@gmail.com>
2026-06-17 18:31:58 +0530

    array

--- public/Array.cpp
+++ public/Array.cpp
@@ -1020,6 +1020,28 @@ vector<int> rearrangeArray(vector<int>& nums) {
 
         return ans;
     }
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+        int left=0,right=tokens.size()-1;
+        int score=0;
+        int ans=0;
+        while(left<=right){
+            if(power>=tokens[left]){
+                power-=tokens[left];
+                score++;
+                ans=max(ans,score);
+                left++;
+            }
+            else if(score>0){
+                power+=tokens[right];
+                score--;
+                right--;
+            }
+            else{break;}
+        }
+        return ans;
+        
+    }
 };
 // rearrange array by sign
 
