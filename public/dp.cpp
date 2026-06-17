7d612b8ab82c8446040b673067c55066633dc12a
Gayna Chawla <gaynachawla@gmail.com>
2026-06-17 23:56:37 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -419,4 +419,24 @@ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
 
         
-    }
+    }int f(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+    int n = triangle.size();
+
+    // base case: last row
+    if(i == n-1) return triangle[i][j];
+
+    if(dp[i][j] != -1) return dp[i][j];
+
+    int down = triangle[i][j] + f(i+1, j, triangle, dp);
+    int diag = triangle[i][j] + f(i+1, j+1, triangle, dp);
+
+    return dp[i][j] = min(down, diag);
+}
+
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n = triangle.size();
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+
+    return f(0, 0, triangle, dp);
+}
+    
