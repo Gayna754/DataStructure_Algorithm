f1768fc26b1132074bf9c63712727f78c34cfce1
Gayna Chawla <gaynachawla@gmail.com>
2026-06-18 23:57:52 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -531,7 +531,7 @@ int minimumTotal(vector<vector<int>>& triangle) {
                 else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
             }
         }
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-3;i>=0;i--){
             for(int j1=0;j1<m;j1++){
                 for(int j2=0;j2<m;j2++){
                     int maxi=-1e9;
