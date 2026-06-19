7d2a7b01727136766a8470273f621e14b1bdbdf2
Gayna Chawla <gaynachawla@gmail.com>
2026-06-19 23:05:11 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -639,4 +639,31 @@ int minimumTotal(vector<vector<int>>& triangle) {
 
         return dp[n-1][sum];
     }
+    int find(int day,int last,vector<vector<int>>&mat,vector<vector<int>>&dp){
+        if(day==0){
+            int maxi=0;
+            for(int task=0;task<3;task++){
+                if(task!=last)
+            maxi=max(maxi,mat[day][task]);
+        }
+        return maxi;
+    }
+    if(dp[day][last]!=-1)return dp[day][last];
+    int maxi=0;
+    for(int task=0;task<3;task++){
+        if(task!=last){
+            int curr=mat[day][task]+find(day-1,task,mat,dp);
+            maxi=max(maxi,curr);
+        }
+    }
+   return dp[day][last] = maxi;
+    }
+   
+    int maximumPoints(vector<vector<int>>& mat) {
+        int n=mat.size();
+       vector<vector<int>> dp(n, vector<int>(4, -1));
+       return find(n-1,3,mat,dp);
+    
+        
+    }
     
