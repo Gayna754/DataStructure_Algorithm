cbc06b18ab9001a0ea44f0d16c41a4087f2d8731
Gayna Chawla <gaynachawla@gmail.com>
2026-06-19 23:46:50 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -691,4 +691,41 @@ int minimumTotal(vector<vector<int>>& triangle) {
 
         
     }
+     bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int x : nums) sum += x;
+
+        if (sum % 2) return false;
+
+        int target = sum / 2;
+        int n = nums.size();
+
+        vector<bool> prev(target + 1, false);
+
+        prev[0] = true;
+
+        if (nums[0] <= target)
+            prev[nums[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+
+            vector<bool> curr(target + 1, false);
+            curr[0] = true;
+
+            for (int t = 1; t <= target; t++) {
+
+                bool notTake = prev[t];
+
+                bool take = false;
+                if (nums[i] <= t)
+                    take = prev[t - nums[i]];
+
+                curr[t] = take || notTake;
+            }
+
+            prev = curr;
+        }
+
+        return prev[target];
+    }
     
