20381470833110c84c9aad3906db06c63b46dc9a
Gayna Chawla <gaynachawla@gmail.com>
2026-06-19 18:56:26 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -608,4 +608,35 @@ int minimumTotal(vector<vector<int>>& triangle) {
        
         
     }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+
+        int n = arr.size();
+
+        vector<vector<bool>> dp(
+            n, vector<bool>(sum+1, false));
+
+        // target = 0
+        for(int i=0;i<n;i++)
+            dp[i][0] = true;
+
+        // first row
+        if(arr[0] <= sum)
+            dp[0][arr[0]] = true;
+
+        for(int i=1;i<n;i++) {
+
+            for(int target=1; target<=sum; target++) {
+
+                bool notpick = dp[i-1][target];
+
+                bool pick = true;
+                if(arr[i] <= target)
+                    pick = dp[i-1][target+arr[i]];
+
+                dp[i][target] = pick || notpick;
+            }
+        }
+
+        return dp[n-1][sum];
+    }
     
