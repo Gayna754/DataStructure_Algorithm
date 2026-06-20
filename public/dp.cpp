7db886e2f3a26ade7f33f1fb83b07ca0c6f2c1cc
Gayna Chawla <gaynachawla@gmail.com>
2026-06-20 22:57:24 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -767,4 +767,47 @@ int minimumTotal(vector<vector<int>>& triangle) {
 
         return mini;
     }
+    class Solution {
+public:
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+
+        int sum = 0;
+        for (int x : arr) sum += x;
+
+        vector<bool> prev(sum + 1, false);
+        prev[0] = true;
+
+        if (arr[0] <= sum)
+            prev[arr[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            vector<bool> cur(sum + 1, false);
+            cur[0] = true;
+
+            for (int target = 1; target <= sum; target++) {
+
+                bool notTake = prev[target];
+
+                bool take = false;
+                if (arr[i] <= target)
+                    take = prev[target - arr[i]];
+
+                cur[target] = take || notTake;
+            }
+
+            prev = cur;
+        }
+
+        int mini = INT_MAX;
+
+        for (int s1 = 0; s1 <= sum / 2; s1++) {
+            if (prev[s1]) {
+                mini = min(mini, sum - 2 * s1);
+            }
+        }
+
+        return mini;
+    }
+};
     
