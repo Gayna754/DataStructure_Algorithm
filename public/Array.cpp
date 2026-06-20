4bb589a2138226eabb011a8fd74fe1e7e75be037
Gayna Chawla <gaynachawla@gmail.com>
2026-06-20 19:20:09 +0530

    array

--- public/Array.cpp
+++ public/Array.cpp
@@ -1050,6 +1050,24 @@ vector<int> rearrangeArray(vector<int>& nums) {
 
         
     }
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
+        }
+       
+        while(pq.size()>1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(x!=y)
+            pq.push(x-y);;
+        }
+        if(pq.empty())return 0;
+        else return pq.top();
+        
+    }
 };
 // rearrange array by sign
 
