e182ef29184dd286db5ef3947b0cd643d874fab8
Gayna Chawla <gaynachawla@gmail.com>
2026-06-21 23:36:52 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -935,7 +935,7 @@ int minimumDifference(vector<int>& nums) {
                     --it;
                     long long chosen = s1 + *it;
                     ans = min(ans,
-                              llabs(total - 3 * chosen));
+                              llabs(total - 4 * chosen));
                 }
             }
         }
