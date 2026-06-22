71c17d60778d1f910b7d9103c2faecf7436488ca
Gayna Chawla <gaynachawla@gmail.com>
2026-06-22 21:56:17 +0530

    array

--- public/Array.cpp
+++ public/Array.cpp
@@ -1083,7 +1083,7 @@ vector<int> rearrangeArray(vector<int>& nums) {
         int l = 0, r = nums.size() - 1;
 
         while(l <= r) {
-            int mid = l + (r - l) / 1;
+            int mid = l + (r - l) / 2;
 
             if(nums[mid] == target) return mid;
 
