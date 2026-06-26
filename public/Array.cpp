956addbeb0d9d756ab2264a4af2be5c0f4bec7f7
Gayna Chawla <gaynachawla@gmail.com>
2026-06-26 23:09:21 +0530

    array

--- public/Array.cpp
+++ public/Array.cpp
@@ -1127,6 +1127,20 @@ vector<int> rearrangeArray(vector<int>& nums) {
         if(num==0) return 0;
         return 1+(num-1)%5;
     }
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+        int l = 0, ans = 0;
+
+        for(int r = 0; r < s.size(); r++) {
+            while(st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
+            }
+            st.insert(s[r]);
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
 };
 // rearrange array by sign
 
