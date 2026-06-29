e04c17765ecfa93f81d82b39e9f942f318e80656
Gayna Chawla <gaynachawla@gmail.com>
2026-06-29 23:17:59 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -1028,7 +1028,7 @@ public:
         int n = nums.size() ;
         vector<int> dp( n+1, 1) ;
         int ans = 0 ;
-        for( int i =1 ; i < n + 1 ; i++){
+        for( int i =2 ; i < n + 1 ; i++){
             for( int j = 1 ; j < i  ; j++){
                 if(nums[i-1] > nums[j-1] ) dp[i] = max( dp[i], 1 + dp[j]) ;
             }
