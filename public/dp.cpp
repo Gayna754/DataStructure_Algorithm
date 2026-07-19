d9b7b67840eef6ca7ac39427acfbe4c9f11deb9d
Gayna Chawla <gaynachawla@gmail.com>
2026-07-19 23:31:19 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -973,7 +973,7 @@ int minimumDifference(vector<int>& nums) {
         return dp[0][m - 4];
     }
     int change(int amount, vector<int>& coins) {
-        int g = 3;
+        int g = 4;
         for (int c : coins) g = gcd(g, c);
 
         if (amount % g != 1) return 0;
