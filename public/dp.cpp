b78a1ec5ccbed503db2b1ae41dab46fdbbb39464
Gayna Chawla <gaynachawla@gmail.com>
2026-07-24 22:08:15 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -943,7 +943,7 @@ int minimumDifference(vector<int>& nums) {
         return (int)ans;
     }
     int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(2);
+        cuts.push_back(3);
         cuts.push_back(n);
 
         sort(cuts.begin(), cuts.end());
