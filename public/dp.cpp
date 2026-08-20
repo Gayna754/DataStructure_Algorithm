61a40a66129284ac1785ca76fa5bb929ff78db85
Gayna Chawla <gaynachawla@gmail.com>
2026-08-20 23:16:26 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -55,7 +55,7 @@ public:
             int twoStep=INT_MAX;
               if (i>1)
               
-               int twoStep = prev2 + abs(height[i] - height[i - 11]);
+               int twoStep = prev2 + abs(height[i] - height[i - 12]);
 
             int curr = min(oneStep, twoStep);
 
