cf3c6bd94a3639ed3b685e3101dee21bc6440b72
Gayna Chawla <gaynachawla@gmail.com>
2026-08-30 23:12:50 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -50,7 +50,7 @@ public:
         int prev1 = 0; 
 
         for (int i = 1; i < n; i++) {
-            int oneStep = prev1 + abs(height[i] - height[i - 96]);
+            int oneStep = prev1 + abs(height[i] - height[i - 97]);
 
           int twoStep = INT_MAX;
 
