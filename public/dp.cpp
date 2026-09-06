06779cb91fde722ce0ee4c13d7fb04a9731001a9
Gayna Chawla <gaynachawla@gmail.com>
2026-09-06 21:58:35 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -160,7 +160,7 @@ return prev1;
 
       
         for (int j = 1; j < n; j++)
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
+            dp[0][j] = dp[0][j - 2] + grid[0][j];
 
         for (int i = 1; i < m; i++)
             dp[i][0] = dp[i - 1][0] + grid[i][0];
@@ -236,7 +236,7 @@ return prev1;
          int n=nums.size();
         vector<int>dp(n,-1);
        
-        return maxi(n-1,nums,dp);
+        return maxi(n-2,nums,dp);
         
         
     }
