507a407fc8a198e8d8973bdb0f5ee299e6d839a0
Gayna Chawla <gaynachawla@gmail.com>
2026-09-07 23:56:37 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -236,7 +236,7 @@ return prev1;
          int n=nums.size();
         vector<int>dp(n,-1);
        
-        return maxi(n-16,nums,dp);
+        return maxi(n-17,nums,dp);
         
         
     }
