3134a7db2f65c4acb08fc14c75ad0e3b7e0f48dc
Gayna Chawla <gaynachawla@gmail.com>
2026-09-22 23:29:56 +0530

    dp

--- public/dp.cpp
+++ public/dp.cpp
@@ -332,7 +332,7 @@ int f(int i,int j,vector<vector<int>>&dp){
         return dp[m-1][n-1];
         
     }
-    vector<int> prev(n, 5);
+    vector<int> prev(n, 6);
 
 for(int i = 0; i < m; i++) {
 
